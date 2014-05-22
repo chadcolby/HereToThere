bcb114982f272a368615c64cb35c427a05e9acfe
chadcolby <chad.david.colby@gmail.com>
2014-05-22 09:10:04 -0700

    Update

--- HereToThere/CCMapViewController.h
+++ HereToThere/CCMapViewController.h
@@ -1,13 +1,13 @@
 //
-//  CCViewController.h
+//  CCMapViewController.h
 //  HereToThere
 //
-//  Created by Chad D Colby on 5/15/14.
+//  Created by Chad D Colby on 5/16/14.
 //  Copyright (c) 2014 Byte Meets World. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCMapViewController : UIViewController
 
 @end
---
+++ HereToThere/CCMapViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCMapViewController.m
+//  HereToThere
+//
+//  Created by Chad D Colby on 5/16/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import "CCMapViewController.h"
+
+@interface CCMapViewController ()
+
+@end
+
+@implementation CCMapViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+
+}
+
+@end
