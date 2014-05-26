76287002c44191d7498ec39d35582ab257c66a70
chadcolby <chad.david.colby@gmail.com>
2014-05-25 22:17:47 -0700

    Finger-placement view addition

---
+++ HereToThere/CCFingerView.h
@@ -0,0 +1,13 @@
+//
+//  CCFingerView.h
+//  HereToThere
+//
+//  Created by Chad D Colby on 5/25/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFingerView : UIView
+
+@end
---
+++ HereToThere/CCFingerView.m
@@ -0,0 +1,31 @@
+//
+//  CCFingerView.m
+//  HereToThere
+//
+//  Created by Chad D Colby on 5/25/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import "CCFingerView.h"
+
+@implementation CCFingerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
