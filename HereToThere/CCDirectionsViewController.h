114a8b4b9ef35d33a1fbba75ac9b86191e73c284
chadcolby <chad.david.colby@gmail.com>
2014-05-26 13:57:08 -0700

    addition of directions

---
+++ HereToThere/CCDirectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCDirectionsViewController.h
+//  HereToThere
+//
+//  Created by Chad D Colby on 5/26/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCDirectionsViewController : UICollectionViewController
+
+@end
---
+++ HereToThere/CCDirectionsViewController.m
@@ -0,0 +1,49 @@
+//
+//  CCDirectionsViewController.m
+//  HereToThere
+//
+//  Created by Chad D Colby on 5/26/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import "CCDirectionsViewController.h"
+
+@interface CCDirectionsViewController ()
+
+@end
+
+@implementation CCDirectionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
